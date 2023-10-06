3156d3e874c156d319defbaf66df13d21aa0e8e2
Mary <omota4fidelity@gmail.com>
2023-10-06 22:36:30 +0100

    updated

--- lib/vehicle.py
+++ lib/vehicle.py
@@ -1,2 +1,12 @@
 class Vehicle:
+    def __init__(self, wheel_size, wheel_number):
+        self.wheel_size = wheel_size
+        self.wheel_number = wheel_number
+        pass
+
+    def go (self):
+        return "vrrrrrrrooom!"
     pass
+
+    def fill_up_tank(self):
+        return "filling up!"
